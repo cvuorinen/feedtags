d321643c801435d16e65fb755788e99a3afaf0ac
Carl Vuorinen <carl.vuorinen@gmail.com>
2014-04-16 17:29:38 +0300

    Feed item list UI WIP

--- src/Feedtags/FrontendBundle/Resources/public/js/controllers.js
+++ src/Feedtags/FrontendBundle/Resources/public/js/controllers.js
@@ -3,5 +3,11 @@ angular.module('feedtags.controllers', [])
         // Fetch feeds from server
         $scope.feeds = Feed.query();
     }).controller('FeedItemsController', function ($scope, feedItems) {
+        $scope.itemListType = 'items-list-compact';
+
         $scope.feedItems = feedItems;
+
+        $scope.alert = function (msg) {
+            alert(msg);
+        };
     });
